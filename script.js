972085a20777c383f712a07dbaeb21d254149dc3
IcedYukino <IcedYukino@outlook.com>
2026-03-06 20:41:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,23 @@ dropdown.style.display = "block";
 }
 
 }
+function searchSongs() {
+
+const input = document.getElementById("songSearch").value.toLowerCase();
+
+const songs = document.querySelectorAll(".song");
+
+songs.forEach(song => {
+
+const title = song.querySelector("h3").innerText.toLowerCase();
+const artist = song.querySelector(".artist").innerText.toLowerCase();
+
+if(title.includes(input) || artist.includes(input)){
+song.style.display = "block";
+}else{
+song.style.display = "none";
+}
+
+});
+
+}
