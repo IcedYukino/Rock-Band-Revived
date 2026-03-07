869fbd5100471108e119e90dac0a117e669ed20a
IcedYukino <IcedYukino@outlook.com>
2026-03-06 20:31:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function toggleDropdown(img) {
+function toggleDropdown(img){
 
-    const song = img.closest(".song");
-    const dropdown = song.querySelector(".dropdown");
+const song = img.parentElement;
+const dropdown = song.querySelector(".dropdown");
 
-    // close all other dropdowns
-    document.querySelectorAll(".dropdown").forEach(d => {
-        if (d !== dropdown) {
-            d.style.display = "none";
-        }
-    });
+/* close other open dropdowns */
+document.querySelectorAll(".dropdown").forEach(d => {
+if(d !== dropdown){
+d.style.display = "none";
+}
+});
 
-    // toggle clicked one
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "block";
-    }
+/* toggle clicked dropdown */
+if(dropdown.style.display === "block"){
+dropdown.style.display = "none";
+} else {
+dropdown.style.display = "block";
+}
 
 }
